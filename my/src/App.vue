e7a5d2ecf115641ff8bc96dfb8b72f8221d24e92
BaldwinTong <765160998@qq.com>
2019-09-02 22:46:34 +0800

    主导航

--- my/src/App.vue
+++ my/src/App.vue
@@ -1,28 +1,96 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row class="footer" :gutter="24">
+      <el-col class="footer_col" :span="24">
+        <el-menu class="el-menu-demo" mode="horizontal" router @select="change" >
+          <el-menu-item :index="item.path" v-for="item in menu" :key="item.name">
+            <i :class="item.icon"></i>
+            <p>{{item.title}}</p>
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+    </el-row>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Vue from "vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI);
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      active: "/home",
+      menu: [
+        { name: "home", title: "首页", path: "/home", icon: "el-icon-s-home" },
+        {
+          name: "discover",
+          title: "找货",
+          path: "/discover",
+          icon: "el-icon-search"
+        },
+        {
+          name: "cart",
+          title: "购物车",
+          path: "/cart",
+          icon: "el-icon-shopping-cart-2"
+        },
+        {
+          name: "mine",
+          title: "个人中心",
+          path: "/mine",
+          icon: "el-icon-s-custom"
+        }
+      ]
+    };
+  },
+  methods: {
+    change(index) {
+      this.active = index;
+    }
+  },
+  created() {
+    this.active = this.$route.path;
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  position: relative;
+}
+.footer_col {
+  position: fixed;
+  bottom: 0px;
+  left: 0px;
+}
+.footer_col li {
+  width: 85px;
+  padding-left: 0px !important;
+  padding-right: 0px !important;
+}
+.el-col-6 {
+  width: 93px;
+}
+.el-row {
+  width: 100%;
+}
+.el-menu-item {
+  font-size: 12px;
+  line-height: 0px !important;
+  border: none !important;
+}
+.el-menu-demo{
+  border: none;
+  
 }
 </style>
--- my/src/router/index.js
+++ my/src/router/index.js
@@ -1,105 +1,33 @@
-import Vue from 'vue';
+import Vue from 'vue'
+import Home from '../components/Home.vue'
+import Cart from '../components/Cart.vue'
+import List from '../components/List.vue'
+import Detail from '../components/Detail.vue'
+import Login from '../components/Login.vue'
+import Register from '../components/Register.vue'
+import Mine from '../components/Mine.vue'
+import Discover from '../components/Discover.vue'
 
-import Home from '../pages/Home.vue'
-import Cart from '../pages/Cart.vue'
-import Login from '../pages/Login.vue'
-import Reg from '../pages/Reg.vue'
-import Discover from '../pages/Discover.vue'
-import Mine from '../pages/Mine.vue'
-import Goods from '../pages/Goods.vue'
 
 
-// 路由
-// 1.引入路由
 import VueRouter from 'vue-router';
 
-// 2.安装（使用）路由
-Vue.use(VueRouter);
-
-// 3. 实例化router并配置参数
+Vue.use(VueRouter)
 let router = new VueRouter({
-    // mode:'history',// 默认hash
-    routes: [{
-        // 当浏览器路径为/home时，渲染Home组件内容
-        name: 'home',
-        path: '/home',
-        component: Home
-    }, {
-        // 当浏览器路径为/，重定向到/home
-        path: '/',
-        redirect: { name: 'home' }
-        //  component:Home,
-    }, {
-        name: 'cart',
-        path: '/cart',
-        component: Cart,
-
-        // 路由独享的守卫
-        // beforeEnter(to,from,next){
-        //     console.log('beforeEnter',to,from);
-        //     next()
-        // }
-    }, {
-        name: 'login',
-        path: '/login',
-        component: Login
-    }, {
-        name: 'mine',
-        path: '/mine',
-        component: Mine,
-
-        // 定义组件时传参
-        // props:{a:100,b:200}, //等效于<Mine v-bind="{a:100,b:200}"/> => <Mine v-bind:a="100" v-bind:b="200"/>
-        // props:true, // 等效于<Mine v-bind="$route.params"/>
-        // props:function($route){return {}}
-    }, {
-        name: 'discover',
-        path: '/discover',
-        component: Discover,
-
-        // 嵌套路由
-        // children:[
-        //     // 路径为 /discover/shuangzhuang 时，渲染shuangzhuang组件
-        //     {
-        //         name: 'sz',
-        //         path: 'shangzhuang',
-        //         // component: shuangzhuang
-
-        //         // 命名路由
-        //         components:{
-        //             title:shangzhuangTile,
-        //             list:shangzhuang
-        //         }
-        //     },
-        //     {
-        //         name: 'xz',
-        //         path: 'xiazhuang',
-        //         component: xiazhuang
-        //     }
-        // ]
-    }, {
-        name: 'reg',
-        path: '/reg',
-        component: Reg
-    }, {
-        // 动态路由，id变量保存在params
-        name: 'goods',
-        path: '/goods/:id',
-        component: Goods
-    }]
-});
-
-// 路由拦截
-router.beforeEach(function(to,from,next){
-    console.log('beforeEach',to,from);
-    // if(已登录)
-    next();
-
-    // else
-    // next({
-        // path:'/login',
-        // query:{target:to.fullPath}
-    // })
+    routes: [
+        { name:'', path: '/', redirect: '/home' },
+        { name:'home', path: '/home', component: Home },
+        { name:'cart', path: '/cart', component: Cart },
+        { name:'list', path: '/list/:id', component: List },
+        { name:'login', path: '/login', component: Login },
+        { name:'register', path: '/register', component: Register },
+        { name:'mine', path: '/mine', component: Mine},
+        { name:'discover', path: '/discover', component: Discover },
+        { name:'detail', path: '/detail', component: Detail },
+    ]
 })
-
+// router.beforeEach((to,from,next)=>{
+//     next()
+// })
 export default router;
+
